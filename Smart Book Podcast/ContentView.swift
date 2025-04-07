5e86a7b8a07fffedbde37895c6084337f664cb5c
Andrii Halabuda <ahalabuda@diligent.com>
2025-04-07 11:12:48 -0400

    Initial Commit

---
+++ Smart Book Podcast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smart Book Podcast
+//
+//  Created by Andrii Halabuda on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smart Book Podcast/Smart_Book_PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Book_PodcastApp.swift
+//  Smart Book Podcast
+//
+//  Created by Andrii Halabuda on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Book_PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
